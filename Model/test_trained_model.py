48bd649eb09024619b191abe75b4f22d1152d42d
nowakkamil <nowakkamil@yahoo.com>
2020-05-16 15:16:42 +0200

    Incorporate data frames in the test_trained_model.py

--- Model/test_trained_model.py
+++ Model/test_trained_model.py
@@ -7,6 +7,7 @@ from data_frame_selector import DataFrameSelector
 from categorical_encoder import CategoricalEncoder
 
 import numpy as np
+import pandas as pd
 
 
 CURRENT_DIR = Path(__file__).parent.absolute()
@@ -17,8 +18,48 @@ model = joblib.load(FULL_PATH)
 
 # Test data
 # Expected output: "no"
-test_data = [[34, "technician", "married", "secondary", "no", 1026,
-              "no", "no", "cellular", 12, "nov", 319, 1, 100, 6, "failure"]]
+test_data = [34, "technician", "married", "secondary", "no", 1026,
+             "no", "no", "cellular", 12, "nov", 319, 1, 100, 6, "failure"]
+
+columns = ["age",
+           "job",
+           "marital",
+           "education",
+           "default",
+           "balance",
+           "housing",
+           "loan",
+           "contact",
+           "day",
+           "month",
+           "duration",
+           "campaign",
+           "pdays",
+           "previous",
+           "poutcome"]
+
+data = {"age": [34],
+        "job": ["technician"],
+        "marital": ["married"],
+        "education": ["secondary"],
+        "default": ["no"],
+        "balance": [1026],
+        "housing": ["no"],
+        "loan": ["no"],
+        "contact": ["cellular"],
+        "day": [12],
+        "month": ["nov"],
+        "duration": [319],
+        "campaign": [1],
+        "pdays": [100],
+        "previous": [6],
+        "poutcome": ["failure"]}
+
+
+# Create the pandas DataFrame
+df = pd.DataFrame(data, columns=columns)
+
+print(df)
 
 # Pipelines definition
 numerical_pipeline = Pipeline([
@@ -38,7 +79,8 @@ preprocess_pipeline = FeatureUnion(transformer_list=[
     ("categorical_pipeline", categorical_pipeline),
 ])
 
-X_test = preprocess_pipeline.fit_transform(test_data)
+X_test = preprocess_pipeline.fit_transform(df)
+print(X_test)
 
 output = model.predict(X_test)
 print(output)
