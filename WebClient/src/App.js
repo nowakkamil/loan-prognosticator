0b53ad9d99349a94f321e2241543e8adf90df0aa
Marcin Wrotecki <marcin.wrotecki.97@gmail.com>
2020-06-04 00:59:53 +0200

    Add nav-bar with styling

--- WebClient/src/App.js
+++ WebClient/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Start from './Start';
+import NavBar from './navBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <Start />
       </header>
--- WebClient/src/Start.js
+++ WebClient/src/Start.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
-import Form from './Form';
-import Button from './Button';
+import React, { Component } from "react";
+import Form from "./Form";
+import Button from "./Button";
 
 class Start extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: true,
-        };
-        this._onButtonClick = this._onButtonClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: true,
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
+  }
 
-    _onButtonClick() {
-        this.setState({
-            value: !this.state.value
-        })
-    }
+  _onButtonClick() {
+    this.setState({
+      value: !this.state.value,
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <p>
-                    Welcome to Bank App
-                </p>
-                {this.state.value ?
-                    <Button handler = {this._onButtonClick} /> :
-                    <Form handler = {this._onButtonClick}/>
-                }
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p>Welcome to Bank App</p>
+        {this.state.value ? (
+          <Button handler={this._onButtonClick} />
+        ) : (
+          <Form handler={this._onButtonClick} />
+        )}
+      </div>
+    );
+  }
 }
 export default Start;
-
